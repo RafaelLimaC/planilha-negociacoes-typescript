424961a50b9ab7d8dcd15bd62a877a0e9dfd8bf7
RafaelLimaC <srafalimacosta@gmail.com>
2023-07-10 16:34:34 -0300

    Isolando código

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,19 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
+        const regex = /-/g;
+        const date = new Date(this.inputData.value.replace(regex, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+}
